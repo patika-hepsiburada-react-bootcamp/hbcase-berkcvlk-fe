5291a3a88de900cdcb2296ec4e75a5a04d620f07
Berk Civelek <berkcvlk@gmail.com>
2021-10-28 00:47:18 +0300

    add: color, brand filter toggle functions

--- src/contexts/queries.js
+++ src/contexts/queries.js
@@ -11,17 +11,41 @@ const Provider = ({ children }) => {
   const [order, setOrder] = useState("");
   const [search, setSearch] = useState("");
 
+  const stringifyFilter = () => {
+    return "brand:apple,huawei;color:kirmizi";
+  };
+
+  const toggleBrand = (brandValue) => {
+    if (brand.includes(brandValue)) {
+      return setBrand((prev) => prev.filter((br) => br !== brand));
+    }
+
+    setBrand((prev) => [...prev, brandValue]);
+  };
+
+  const toggleColor = (colorValue) => {
+    if (color.includes(colorValue)) {
+      return setColor((prev) => prev.filter((clr) => clr !== colorValue));
+    }
+
+    setColor((prev) => [...prev, colorValue]);
+  };
+
   return (
     <QueriesContext.Provider
       value={{
         brand,
-        setBrand,
+        toggleBrand,
         color,
-        setColor,
+        toggleColor,
         order,
         setOrder,
         search,
         setSearch,
+        query: {
+          filter: stringifyFilter(),
+          search: search,
+        },
       }}
     >
       {children}
