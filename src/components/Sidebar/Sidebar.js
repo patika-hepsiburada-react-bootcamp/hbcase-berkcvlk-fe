88b1a5f78ca711444ed024eca35c0d6a9a215d4d
Berk Civelek <berkcvlk@gmail.com>
2021-10-28 11:25:53 +0300

    add: filters

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,20 @@
+import { useFilters, useQueries } from "hooks";
 import Filter from "./Filter/Filter";
 import * as S from "./Sidebar.styled";
 
 const Sidebar = () => {
+  const { filters } = useFilters();
+  const { color, order, brand } = useQueries();
+
   return (
     <S.Sidebar>
-      <Filter />
+      {filters.map((filter, index) => (
+        <Filter
+          key={index}
+          filter={filter}
+          activeStates={[color, order, brand][index]}
+        />
+      ))}
     </S.Sidebar>
   );
 };
