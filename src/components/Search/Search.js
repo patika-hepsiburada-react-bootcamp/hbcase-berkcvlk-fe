f4d2c7849ce90ffe20e756b4182388b4c05025d8
Berk Civelek <berkcvlk@gmail.com>
2021-10-28 13:36:51 +0300

    add: search state to input

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
+
+import { useQueries } from "hooks";
 import * as S from "./Search.styled";
 
 const Search = () => {
+  const [localSearch, setLocalSearch] = useState("");
+  const { setSearch } = useQueries();
+
+  useEffect(() => {
+    if (!localSearch || localSearch.length < 2) {
+      return;
+    }
+
+    // Give a time between each key stroke
+    const setSearchTimeout = setTimeout(() => setSearch(localSearch), 300);
+
+    return () => clearTimeout(setSearchTimeout);
+  }, [localSearch]);
+
+  const searchChangeHandler = (event) => {
+    setLocalSearch(event.target.value);
+  };
+
   return (
     <S.Wrapper>
       <S.SearchIcon />
-      <S.Input placeholder="25 milyon'dan fazla ürün içerisinde ara" />
+      <S.Input
+        onChange={searchChangeHandler}
+        value={localSearch}
+        placeholder="25 milyon'dan fazla ürün içerisinde ara"
+      />
     </S.Wrapper>
   );
 };
