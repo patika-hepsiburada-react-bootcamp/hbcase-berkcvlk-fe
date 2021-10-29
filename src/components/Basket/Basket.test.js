43fb66c2aab792f2447d932106533a1f3a9b5876
Berk Civelek <berkcvlk@gmail.com>
2021-10-29 16:39:33 +0300

    test: basket

---
+++ src/components/Basket/Basket.test.js
@@ -0,0 +1,56 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { BasketContext } from "contexts/basket";
+import { ModalContext } from "contexts/modal";
+import Basket from "./Basket";
+
+describe("Basket tests", () => {
+  let anchor;
+
+  let basketItems = [
+    { productId: 1, image: "/images/prod1.png", title: "Basket Item" },
+    { productId: 2, image: "/images/prod1.png", title: "Basket Item" },
+  ];
+
+  beforeEach(() => {
+    render(
+      <BasketContext.Provider value={{ basketItems }}>
+        <ModalContext.Provider value={{ showModal: jest.fn() }}>
+          <Basket />
+        </ModalContext.Provider>
+      </BasketContext.Provider>
+    );
+
+    anchor = screen.getByText("Sepetim");
+  });
+
+  test("anchor should be in the document", () => {
+    expect(anchor).toBeInTheDocument();
+  });
+
+  test("hover over the anchor should open the basket menu", () => {
+    userEvent.hover(anchor);
+
+    const basketItems = screen.getAllByText("Basket Item");
+
+    expect(basketItems).toHaveLength(2);
+  });
+});
+
+// Avoid before each rendering
+describe("", () => {
+  test("if there is no basket item text should be in the document", () => {
+    const { getByText } = render(
+      <BasketContext.Provider value={{ basketItems: [] }}>
+        <Basket />
+      </BasketContext.Provider>
+    );
+
+    const anchor = getByText("Sepetim");
+    userEvent.hover(anchor);
+
+    const noItem = getByText("Sepetinizde ürün bulunmamaktadır!");
+    expect(noItem).toBeInTheDocument();
+  });
+});
