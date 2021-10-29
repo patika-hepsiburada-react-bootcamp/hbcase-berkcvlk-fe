5c4df44ee21b1df02c916630c9845ff22f8d4bc5
Berk Civelek <berkcvlk@gmail.com>
2021-10-30 00:03:03 +0300

    test: search

---
+++ src/components/Search/Search.test.js
@@ -0,0 +1,26 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Search from "./Search";
+import { QueriesContext } from "contexts/queries";
+
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
+
+describe("Search input tests", () => {
+  test("should call setter one when type 3 chars", () => {
+    const setSearch = jest.fn();
+
+    render(
+      <QueriesContext.Provider value={{ setSearch }}>
+        <Search />
+      </QueriesContext.Provider>
+    );
+
+    const input = screen.getByPlaceholderText(/25 milyon/i);
+
+    userEvent.type(input, "app");
+
+    setTimeout(() => expect(setSearch).toHaveBeenCalledTimes(1), 300);
+  });
+});
