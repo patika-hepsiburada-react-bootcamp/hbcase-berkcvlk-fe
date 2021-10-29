cdfcf233087da90680ba64c46fdc508ef0a4bc54
Berk Civelek <berkcvlk@gmail.com>
2021-10-29 18:39:47 +0300

    change: loading styles

--- src/components/Loading/Loading.styled.js
+++ src/components/Loading/Loading.styled.js
@@ -3,8 +3,7 @@ import styled from "styled-components";
 import theme from "themes";
 
 export const Wrapper = styled.div`
-  width: 65%;
-  margin: 100px 0;
+  width: 100%;
 
   display: flex;
   justify-content: center;
@@ -15,33 +14,36 @@ export const Spinner = styled.div`
   width: 40px;
   height: 40px;
 
+  display: inline-block;
   position: relative;
-  margin: 100px auto;
 `;
 
 export const Bounce = styled.div`
-  width: 100%;
-  height: 100%;
-  border-radius: 50%;
-  background-color: ${theme.colors.orange};
-  opacity: 0.6;
   position: absolute;
-  top: 0;
-  left: 0;
+  border: 4px solid ${theme.colors.orange};
+  opacity: 1;
+  border-radius: 50%;
 
-  animation: sk-bounce 2s infinite ease-in-out;
+  animation: sk-bounce 1s infinite ease-in-out;
 
   @keyframes sk-bounce {
-    0%,
-    100% {
-      -webkit-transform: scale(0);
+    0% {
+      top: 36px;
+      left: 36px;
+      width: 0;
+      height: 0;
+      opacity: 1;
     }
-    50% {
-      -webkit-transform: scale(1);
+    100% {
+      top: 0px;
+      left: 0px;
+      width: 72px;
+      height: 72px;
+      opacity: 0;
     }
   }
 `;
 
 export const Bounce2 = styled(Bounce)`
-  animation-delay: -1s;
+  animation-delay: -0.5s;
 `;
