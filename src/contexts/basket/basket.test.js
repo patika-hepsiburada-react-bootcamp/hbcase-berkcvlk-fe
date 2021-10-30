d59bd1e8246d2ec29465852b017192abf8ede0bf
Berk Civelek <berkcvlk@gmail.com>
2021-10-30 19:09:15 +0300

    refactor: add beforeeach

--- src/contexts/basket/basket.test.js
+++ src/contexts/basket/basket.test.js
@@ -3,19 +3,23 @@ import { render } from "@testing-library/react";
 import BasketProvider, { BasketContext } from "./basket";
 
 describe("Basket Context tests", () => {
-  test("default values", () => {
-    let items;
+  let items, loading;
 
+  beforeEach(() => {
     render(
       <BasketProvider>
         <BasketContext.Consumer>
-          {({ basketItems }) => {
+          {({ basketItems, isLoading }) => {
             items = basketItems;
+            loading = isLoading;
           }}
         </BasketContext.Consumer>
       </BasketProvider>
     );
+  });
 
+  test("default values", () => {
     expect(items).toHaveLength(0);
+    expect(loading).toBeTruthy();
   });
 });
