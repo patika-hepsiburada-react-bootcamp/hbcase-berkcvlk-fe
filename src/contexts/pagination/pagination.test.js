674ed261c09d793af1c1fb9e436e7cb567f2bb74
Berk Civelek <berkcvlk@gmail.com>
2021-10-30 18:26:22 +0300

    add: page count calculation test

--- src/contexts/pagination/pagination.test.js
+++ src/contexts/pagination/pagination.test.js
@@ -1,25 +1,49 @@
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 
 import PaginationProvider, { PaginationContext } from "./pagination";
 
 describe("Pagination Context tests", () => {
-  test("default values", () => {
-    let defaultPage, defaultProductPerPage, defaultTotalProductCount;
+  let defaultPage,
+    defaultProductPerPage,
+    defaultTotalProductCount,
+    testSetTotalProductCount,
+    testSetProductPerPage,
+    testPageCount;
 
+  beforeEach(() => {
     render(
       <PaginationProvider>
         <PaginationContext.Consumer>
-          {({ page, productPerPage, totalProductCount }) => {
+          {({
+            page,
+            productPerPage,
+            totalProductCount,
+            pageCount,
+            setTotalProductCount,
+            setProductPerPage,
+          }) => {
             defaultPage = page;
             defaultProductPerPage = productPerPage;
             defaultTotalProductCount = totalProductCount;
+            testSetProductPerPage = setProductPerPage;
+            testSetTotalProductCount = setTotalProductCount;
+            testPageCount = pageCount;
           }}
         </PaginationContext.Consumer>
       </PaginationProvider>
     );
+  });
 
+  test("default values", () => {
     expect(defaultPage).toBe(1);
     expect(defaultProductPerPage).toBeUndefined();
     expect(defaultTotalProductCount).toBeUndefined();
+  });
+
+  test("product per page change should change page count", () => {
+    act(() => testSetProductPerPage(1));
+    act(() => testSetTotalProductCount(3));
+
+    expect(testPageCount).toBe(3);
   });
 });
