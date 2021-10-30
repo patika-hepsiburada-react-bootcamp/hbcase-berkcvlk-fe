98b26f16a6808a117218cb7c0f1ce06498a42880
Berk Civelek <berkcvlk@gmail.com>
2021-10-30 18:16:12 +0300

    test: basket context

---
+++ src/contexts/basket/basket.test.js
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+
+import BasketProvider, { BasketContext } from "./basket";
+
+describe("Basket Context tests", () => {
+  test("default values", async () => {
+    let items;
+
+    render(
+      <BasketProvider>
+        <BasketContext.Consumer>
+          {({ basketItems }) => {
+            items = basketItems;
+          }}
+        </BasketContext.Consumer>
+      </BasketProvider>
+    );
+
+    expect(items).toHaveLength(0);
+  });
+});
