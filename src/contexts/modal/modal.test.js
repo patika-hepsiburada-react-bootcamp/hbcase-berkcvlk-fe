5d54ab75c2a2edffddd70382af49d135c9ce92cb
Berk Civelek <berkcvlk@gmail.com>
2021-10-30 18:22:09 +0300

    test: add modal functionallities

--- src/contexts/modal/modal.test.js
+++ src/contexts/modal/modal.test.js
@@ -1,23 +1,40 @@
-import { render } from "@testing-library/react";
+import { act, render } from "@testing-library/react";
 
 import ModalProvider, { ModalContext } from "./modal";
 
 describe("Modal Context tests", () => {
-  test("default values", () => {
-    let defaultIsOpen, defaultProductId;
+  let defaultIsOpen, defaultProductId, testShowModal, testHideModal;
 
+  beforeEach(() => {
     render(
       <ModalProvider>
         <ModalContext.Consumer>
-          {({ isOpen, productId }) => {
+          {({ isOpen, productId, showModal, hideModal }) => {
             defaultIsOpen = isOpen;
             defaultProductId = productId;
+            testShowModal = showModal;
+            testHideModal = hideModal;
           }}
         </ModalContext.Consumer>
       </ModalProvider>
     );
+  });
 
+  test("default values", () => {
     expect(defaultIsOpen).toBeFalsy();
     expect(defaultProductId).toBe(0);
+  });
+
+  test("showModal set isOpen true and set productId", () => {
+    act(() => testShowModal(1));
+
+    expect(defaultIsOpen).toBeTruthy();
+    expect(defaultProductId).toBe(1);
+  });
+
+  test("hide modal set isOpen false", () => {
+    act(() => testHideModal());
+
+    expect(defaultIsOpen).toBeFalsy();
   });
 });
